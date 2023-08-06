b03a2f89bbda9f78498e020b1296fbd0bdbbc2bf
Nitishthakur007 <69647381+Nitishthakur007@users.noreply.github.com>
2023-08-06 21:04:34 +0530

    Add files via upload

---
+++ Inversion of control/09-Bean-Lifecycle-Method/src/test/java/com/nitish/springboot/SpringconstructorinjectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nitish.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringconstructorinjectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ Inversion of control/10-Java-Config-Bean/src/main/java/com/nitish/springboot/common/TennisCoach.java
@@ -0,0 +1,15 @@
+package com.nitish.springboot.common;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class TennisCoach implements Coach{
+
+    public TennisCoach(){
+        System.out.println("In constructor: " + getClass().getSimpleName());
+    }
+    @Override
+    public String getDailyWorkout() {
+        return "Practice Back hand and front hand volley";
+    }
+}
